0bbc12b9087b33c6b863de6e814a72f061926cdd
Mandakini <mandakini@wowtruck.in>
2019-07-14 21:58:04 +0530

    Jest Helper Function beforeEach

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -4,14 +4,15 @@ import App from "components/App";
 import ComponentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
 
+beforeEach( () => {
+  const warraped = shallow(<App />);
+})
 
 it("Show Comment Box", () => {
   //const div = document.createElement("div"); //JSdom libaray comes with jest
-  const warraped = shallow(<App />);
   expect(warraped.find(ComponentBox).length).toEqual(1);
 });
 
 it("Show Comment List", () => {
-  const warraped = shallow(<App />);
   expect(warraped.find(CommentList).length).toEqual(1);
 });
