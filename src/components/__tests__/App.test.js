e5e89195c895c94b64c3399bd2ed911778d3d3b1
Mandakini <mandakini@wowtruck.in>
2019-07-14 22:03:16 +0530

    beforEach only for file not project

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -4,15 +4,17 @@ import App from "components/App";
 import ComponentBox from "components/CommentBox";
 import CommentList from "components/CommentList";
 
-beforeEach( () => {
-  const warraped = shallow(<App />);
-})
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
 
 it("Show Comment Box", () => {
   //const div = document.createElement("div"); //JSdom libaray comes with jest
-  expect(warraped.find(ComponentBox).length).toEqual(1);
+  expect(wrapped.find(ComponentBox).length).toEqual(1);
 });
 
 it("Show Comment List", () => {
-  expect(warraped.find(CommentList).length).toEqual(1);
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
